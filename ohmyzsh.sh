161ee68d538c85bc62dd1045205b482894c19ba2
Defmon3 <111503233+Defmon3@users.noreply.github.com>
2024-04-27 22:33:33 +0200

    added coloring to errors

--- ohmyzsh.sh
+++ ohmyzsh.sh
@@ -16,7 +16,7 @@ highlight "Zsh and plugins installed successfully."
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-git clone https://github.com/zsh-users/zsh-autosuggestions.git "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-autosuggestions.git "$ZSH_CUSTOM/plugins/zsh-autosuggestions" > /dev/null 2>&1
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" > /dev/null 2>&1
 git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git "$ZSH_CUSTOM/plugins/fast-syntax-highlighting" > /dev/null 2>&1
 git clone --depth 1 https://github.com/marlonrichert/zsh-autocomplete.git "$ZSH_CUSTOM/plugins/zsh-autocomplete" > /dev/null 2>&1
@@ -34,15 +34,15 @@ echo -e "source $ZSH_CUSTOM/plugins/zsh-autocomplete/zsh-autocomplete.plugin.zsh
 highlight ".zshrc created and updated successfully."
 
 # Update /etc/zsh/zshenv to ensure it knows about ZDOTDIR
-sudo tee -a /etc/zsh/zshenv <<EOF
-export XDG_CONFIG_HOME="$XDG_CONFIG_HOME"
-export ZDOTDIR="$ZDOTDIR"
-EOF
+
 
 #!/bin/bash
 
 # Define the file path
 ZSHRC="$ZDOTDIR/.zshrc"
+
+echo "export ZDOTDIR=\"$HOME/.config/zsh\"" >> ~/.zshenv
+
 
 sed -i  '/^plugins=(git)$/c\
 plugins=(git zsh-autosuggestions zsh-syntax-highlighting fast-syntax-highlighting zsh-autocomplete)
