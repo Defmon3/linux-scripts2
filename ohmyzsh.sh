11c9e2f887fa8ebeccdb49c02ceb770022bb8769
Defmon3 <111503233+Defmon3@users.noreply.github.com>
2024-04-27 21:34:12 +0200

    added coloring to errors

--- ohmyzsh.sh
+++ ohmyzsh.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -euo pipefail
 
 highlight() { echo -e "\033[1m\033[43m$1\033[0m"; }
 ehighlight() { echo -e "\033[1m\033[41m$1\033[0m"; }
@@ -18,9 +17,9 @@ sudo nala install zsh zsh-autosuggestions zsh-syntax-highlighting -y > /dev/null
 highlight "Zsh and plugins installed successfully."
 
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended  > /dev/null 2>&1
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
 
-git clone https://github.com/zsh-users/zsh-autosuggestions.git "$ZSH_CUSTOM/plugins/zsh-autosuggestions" > /dev/null 2>&1
+git clone https://github.com/zsh-users/zsh-autosuggestions.git "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" > /dev/null 2>&1
 git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git "$ZSH_CUSTOM/plugins/fast-syntax-highlighting" > /dev/null 2>&1
 git clone --depth 1 https://github.com/marlonrichert/zsh-autocomplete.git "$ZSH_CUSTOM/plugins/zsh-autocomplete" > /dev/null 2>&1
