add1565fc143a30b826ac050d03dd30b28677f70
Defmon3 <111503233+Defmon3@users.noreply.github.com>
2024-04-27 14:13:09 +0200

    init

--- main.sh
+++ main.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
+echo "<<< Updating system>>>"
 
 chmod +x ./system-setup.sh
 ./system-setup.sh
 
+echo "<<< Sudo update && upgrade >>>"
 sudo apt-get update -y && sudo apt-get upgrade -y
+
+echo "<<< Installing Nala >>>"
 sudo apt-get install nala  -y
 sudo nala update -y
+
+echo "<<< Installing Curl >>>"
 sudo nala install curl
 
+echo "<<< Installing oh-my-zsh >>>"
 chmod +x ./ohmyzsh.sh
 ./ohmyzsh.sh
 
+echo "<<< Installing sublime >>>"
 chmod +x ./sublime.sh
 ./sublime.sh
 
