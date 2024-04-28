68c8bade02947633a819dffbf3a97f4e283f0590
Defmon3 <111503233+Defmon3@users.noreply.github.com>
2024-04-28 14:55:41 +0200

    added coloring to errors

--- ohmyzsh.sh
+++ ohmyzsh.sh
@@ -4,20 +4,17 @@ highlight() { echo -e "\033[1m\033[43m$1\033[0m"; }
 ehighlight() { echo -e "\033[1m\033[41m$1\033[0m"; }
 
 
-export ZSH="$HOME/.config/oh-my-zsh"
-ZSH_CUSTOM="$HOME/.config/oh-my-zsh/custom"
-ZSH_PLUGINS="$HOME/.config/oh-my-zsh/custom/plugins"
-ZSH_THEMES="$HOME/.config/oh-my-zsh/custom/themes"
+
 sudo nala install zsh zsh-autosuggestions zsh-syntax-highlighting -y
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 mkdir -p "$ZSH_CUSTOM" > /dev/null 2>&1
 
 git clone https://github.com/zsh-users/zsh-autosuggestions.git "$ZSH_PLUGINS/zsh-autosuggestions" > /dev/null 2>&1
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_PLUGINS/zsh-syntax-highlighting" > /dev/null 2>&1
 git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git "$ZSH_PLUGINS/fast-syntax-highlighting" > /dev/null 2>&1
 git clone --depth 1 https://github.com/marlonrichert/zsh-autocomplete.git "$ZSH_PLUGINS/zsh-autocomplete" > /dev/null 2>&1
-git clone --depth 1 https://github.com/romkatv/powerlevel10k.git "$ZSH_THEMES/powerlevel10k"
+
 highlight "Plugins cloned successfully."
 
 
