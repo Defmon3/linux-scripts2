d505d4afcea705779f8540e3183c3795c15ec920
Defmon3 <111503233+Defmon3@users.noreply.github.com>
2024-04-28 14:59:57 +0200

    added coloring to errors

--- ohmyzsh.sh
+++ ohmyzsh.sh
@@ -4,7 +4,9 @@ highlight() { echo -e "\033[1m\033[43m$1\033[0m"; }
 ehighlight() { echo -e "\033[1m\033[41m$1\033[0m"; }
 
 
-
+export ZSH="$HOME/.config/oh-my-zsh"
+ZSH_CUSTOM="$HOME/.config/oh-my-zsh/custom"
+ZSH_PLUGINS="$HOME/.config/oh-my-zsh/custom/plugins"
 sudo nala install zsh zsh-autosuggestions zsh-syntax-highlighting -y
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
