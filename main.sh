717ab5f6691c2467c329ae0d5e46049ebfa42d5e
Defmon3 <111503233+Defmon3@users.noreply.github.com>
2024-04-28 22:04:50 +0200

    added coloring to errors

--- main.sh
+++ main.sh
@@ -1,37 +1,12 @@
 #!/bin/bash
 # main.sh and others
 
-
+PACKAGES=(tmux fzf curl)
 yellow() { echo -e "\033[1m\033[43m$1\033[0m"; }
 
 red() { echo -e "\033[1m\033[41m$1\033[0m"; }
 
 green() { echo -e "\033[1m\033[42m$1\033[0m"; }
-
-debug=1
-execute_command() {
-    local command=$1
-    local local_debug="${2:-$debug}"
-    yellow ">>> $command"
-
-    if [ "$local_debug" -eq 1 ]; then
-        if $command; then
-        green "$command"
-      else
-          red "$command"
-          exit 1
-      fi
-    else
-      if $command >/dev/null 2>&1; then
-        green "$command"
-      else
-          red "$command"
-          exit 1
-      fi
-    fi
-
-
-}
 green "\nStarting main.sh"
 
 #echo $SUDOPASS | sudo -S sh -c "echo '${SUDO_USER:-$(whoami)} ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/temp_nopasswd"
@@ -42,13 +17,19 @@ if [ $? -ne 0 ]; then
 fi
 
 
+yellow "<<< Installing Nala >>>"
 
-execute_command "sudo -S apt install nala -y"
-execute_command "echo $SUDOPASS | sudo -S nala update"
-execute_command "echo $SUDOPASS | sudo -S nala upgrade"
-execute_command "echo $SUDOPASS | sudo -S nala install tmux fzf curl -y"
+echo $SUDOPASS | sudo -S apt install nala -y > /dev/null 2>&1 || { red "Failed to install Nala"; exit 1; }
+echo "Updating Nala..."
+echo $SUDOPASS | sudo -S nala update > /dev/null 2>&1 || { red "Failed to update Nala"; exit 1; }
 
-exit 1
+yellow "<<< Installing $PACKAGES >>>"
+echo $SUDOPASS | sudo -S nala install "${PACKAGES[@]}" -y > /dev/null 2>&1 || {
+    echo "Failed to install $PACKAGES" >&2  # using echo for error message
+    exit 1
+}
+
+
 yellow "<<< Installing terminator >>>"
 chmod +x ./install-terminator.sh
 bash ./install-terminator.sh || { red "oh-my-zsh installation failed"; exit 1; }
@@ -56,6 +37,7 @@ bash ./install-terminator.sh || { red "oh-my-zsh installation failed"; exit 1; }
 yellow "<<< Installing oh-my-zsh >>>"
 chmod +x ./ohmyzsh.sh
 bash ./ohmyzsh.sh || { red "oh-my-zsh installation failed"; exit 1; }
+
 
 yellow "<<< Installing Sublime >>>"
 chmod +x ./sublime.sh
@@ -65,10 +47,3 @@ yellow "<<< Setting up auto login >>>"
 chmod +x ./auto_login.sh
 echo $SUDOPASS | sudo -S bash ./auto_login.sh || { echo 'Auto login setup failed'; exit 1; }
 
-
-
-# reboot
-
-#
-# Speed up Ubuntu boot
-# https://itsfoss.com/speed-up-ubuntu-131
