22568a09475ef179ce73ccfdb539f112fbe963d8
Defmon3 <111503233+Defmon3@users.noreply.github.com>
2024-05-04 15:36:26 +0200

    Working revision

--- install.py
+++ install.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import os
 import sys
 
 if len(sys.argv) < 2:
@@ -6,18 +7,17 @@ if len(sys.argv) < 2:
     sys.exit(1)
 sudopass = sys.argv[1]
 
-import os
 home = os.path.expanduser("~")
 config = os.path.join(home, ".config")
 if not os.path.exists(config):
     os.makedirs(config)
 print("Made dirs")
-sudopass = input("Enter your sudo password: ")
 os.system(f"export SUDOPASS={sudopass}")
 os.system('sudo -S apt update')
 os.system('echo $SUDOPASS | sudo -S apt install git -y > /dev/null 2>&1 &&')
 os.chdir(config)
-os.system('(git clone https://github.com/Defmon3/linux-scripts2.git || (cd linux-scripts2 && git pull))  > /dev/null 2>&1 && ')
+os.system(
+    '(git clone https://github.com/Defmon3/linux-scripts2.git || (cd linux-scripts2 && git pull))  > /dev/null 2>&1 && ')
 os.system('sudo apt autoremove -y > /dev/null 2>&1 &&')
 os.system('sudo apt autoclean -y > /dev/null 2>&1 &&')
 os.system('bash ./main.sh')
--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 cd ~ || exit 1
 #sh -c "$(curl -f https://raw.githubusercontent.com/Defmon3/linux-scripts2/master/install.sh)" | bash
 sh -c "$(curl -f https://raw.githubusercontent.com/Defmon3/linux-scripts2/master/install.py)" | python3
+echo "Enter your sudo password:" && read -s sudopass && wget -qO- https://raw.githubusercontent.com/Defmon3/linux-scripts2/master/install.py | python3 - "$sudopass"
